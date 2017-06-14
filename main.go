60e21333e0ffe4108b23ebd2a23838583c348e07
PaulDorra <paul.dorra@optiopay.com>
2017-06-14 19:09:16 +0200

    Finished work on the first program

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("My name is %s, my age is %d", "Paul", 28)
+	greet("Hello, what is your name?", "Your name is: %s")
+	greet("Konichiwa, how old are you?", "Your age is: %s")
+}
+
+func greet(question string, answer string) {
+	var input string
+	fmt.Printf(question)
+	fmt.Printf("\n")
+	fmt.Scanln(&input)
+	fmt.Printf("\n")
+	fmt.Printf(answer, input)
+	fmt.Printf("\n")
 }
